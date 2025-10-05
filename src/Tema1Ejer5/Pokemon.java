3a9b0a2a5203cf8dae61d274bb5bbd4e91d9a4ed
jojoseja <jfeyjoo@gmail.com>
2025-10-05 16:27:41 +0200

    pokemon class

---
+++ src/Tema1Ejer5/Pokemon.java
@@ -0,0 +1,94 @@
+package Tema1Ejer5;
+
+import java.io.*;
+
+public class Pokemon implements Externalizable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    String name;
+    int level;
+    int life;
+    int attack;
+    int defense;
+    int specialAttack;
+    int specialDefense;
+    int speed;
+
+    public Pokemon() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpecialAttack() {
+        return specialAttack;
+    }
+
+    public void setSpecialAttack(int specialAttack) {
+        this.specialAttack = specialAttack;
+    }
+
+    public int getSpecialDefense() {
+        return specialDefense;
+    }
+
+    public void setSpecialDefense(int specialDefense) {
+        this.specialDefense = specialDefense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+    }
+}
