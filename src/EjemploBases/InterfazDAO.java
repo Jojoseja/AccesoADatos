37aa647f9fac75e8f134e2ea4ac8dc34b71e6566
jojoseja <jfeyjoo@gmail.com>
2025-10-16 10:15:38 +0200

    ejemplobases

---
+++ src/EjemploBases/InterfazDAO.java
@@ -0,0 +1,19 @@
+package EjemploBases;
+
+public interface InterfazDAO {
+    //CRUD Casino
+
+    //Leer Fichero
+    void readFile();
+
+    //Crear - Registrar Usuario
+    boolean createUser();
+
+    //Update - Modificar Usuario
+    boolean updateUser();
+
+    //Delete - Borrar Usuario
+    boolean deleteUser();
+
+
+}
